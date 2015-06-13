60aa0e3cbb33c35098941d54ffc4fe0189c55255
Wispers <vasiliy16_99@mail.ru>
2015-06-13 22:09:59 +0600

    Создал метод

--- Tanks/src/com/core/main.java
+++ Tanks/src/com/core/main.java
@@ -1,18 +1,13 @@
 package com.core;
 
 import javax.swing.JFrame;
-import javax.swing.JPanel;
 
 public class main {
 
 	public static void main(String[] args) {
 	
 		JFrame f = new JFrame("Tanks");
-		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		f.pack();
-		f.setVisible(true); 
-		f.getContentPane();
-		f.add(new JPanel());		
+
 	}
 
 }
