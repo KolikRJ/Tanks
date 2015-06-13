9e4faaf8ab1ad91f8cc49789bb0e95969079b078
Kolik <KolikRJ@yandex.ru>
2015-06-13 22:07:07 +0600

    сп

--- Tanks/src/com/core/main.java
+++ Tanks/src/com/core/main.java
@@ -7,10 +7,6 @@ public class main {
 	public static void main(String[] args) {
 	
 		JFrame f = new JFrame("Tanks");
-		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		f.pack();
-		f.setVisible(true); 
-		f.getContentPane();
 		
 	}
 
