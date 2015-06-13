eb52aade8a6aeb915c6b10c623708de4f7b86ea4
Kolik <KolikRJ@yandex.ru>
2015-06-13 22:03:09 +0600

    Дописал класс Main

--- Tanks/src/com/core/main.java
+++ Tanks/src/com/core/main.java
@@ -7,6 +7,7 @@ public class main {
 	public static void main(String[] args) {
 	
 		JFrame f = new JFrame("Tanks");
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		f.setVisible(true); 
 		
 	}
