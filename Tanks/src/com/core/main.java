5a1d9af4617f7488483a1addf2f6c0dcece97240
Kolik <KolikRJ@yandex.ru>
2015-06-13 22:06:06 +0600

    Удалил

--- Tanks/src/com/core/main.java
+++ Tanks/src/com/core/main.java
@@ -7,9 +7,6 @@ public class main {
 	public static void main(String[] args) {
 	
 		JFrame f = new JFrame("Tanks");
-		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		f.setVisible(true); 
-		f.getContentPane();
 		
 	}
 
